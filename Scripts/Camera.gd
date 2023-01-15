8fbbef6672462c9bf59ac979c9d46091a867c2a7
Nick Cunningham <10342737+TheNickOfTime@users.noreply.github.com>
2023-01-14 16:33:12 -0800

    Created basic 3rd person controller with the ability to swap between controlled characters

--- Scripts/Camera.gd
+++ Scripts/Camera.gd
@@ -1,11 +1,29 @@
 extends SpringArm3D
+class_name PlayerCamera
 
 
-# Called when the node enters the scene tree for the first time.
+var character : CharacterBody3D
+var offset : Vector3 = Vector3(0, 1.8, 0)
+var mouse_delta : Vector2
+var pan_speed : float = 0.25
+var pan_direction : int = -1
+var max_angle : float = 30
+
+
 func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
 	pass
+
+
+func _process(delta):
+	follow_camera(delta)
+	rotate_camera(delta)
+
+
+func follow_camera(delta : float):
+	position = character.position + offset
+
+
+func rotate_camera(delta : float):
+	rotate_y(mouse_delta.x * pan_direction * pan_speed * delta)
+	rotate_object_local(Vector3.RIGHT, mouse_delta.y * pan_direction * pan_speed * delta)
+	rotation.x = clamp(rotation.x, deg_to_rad(-max_angle), deg_to_rad(max_angle))
