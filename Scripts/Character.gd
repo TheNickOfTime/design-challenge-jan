62fe300afc688bb4911146a2d653972d4f6125cd
Nick Cunningham <thenickoftime.x@gmail.com>
2023-01-16 17:42:15 -0800

    Added navigation support for non-primary characters.

--- Scripts/Character.gd
+++ Scripts/Character.gd
@@ -5,35 +5,76 @@ class_name PlayerCharacter
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 
-var move_direction : Vector2
+enum CONTROL_STATE {Input, Navigation}
 
+# On Ready Vars-------------------------------------------------------------------------------------
+@onready var nav_agent : NavigationAgent3D = $NavigationAgent3D
+
+# Member Vars---------------------------------------------------------------------------------------
+var control_state : CONTROL_STATE
+
+var move_direction : Vector2
+var move_speed : float = 5.0
 var rotate_speed : float = 1
 var rotate_direction : Vector2
+
+var nav_destination : Vector3
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
+
+# Bult-In Functions---------------------------------------------------------------------------------
 func _process(delta):
 	rotate_character(delta)
+
 
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y -= gravity * delta
 	
-	move(move_direction)
+	match control_state:
+		CONTROL_STATE.Input:
+			move_input(move_direction)
+		CONTROL_STATE.Navigation:
+			move_nav()
 
-func move(move_direction : Vector2):
-	var direction = (transform.basis * Vector3(move_direction.x, 0, move_direction.y)).normalized()
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+
+# Input Movement Functions--------------------------------------------------------------------------
+func move_input(direction : Vector2):
+	var new_direction = (transform.basis * Vector3(direction.x, 0, direction.y)).normalized()
+	if new_direction:
+		velocity.x = new_direction.x * SPEED
+		velocity.z = new_direction.z * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 
 	move_and_slide()
 
+
+func move_nav():
+	var current_position : Vector3 = global_transform.origin
+	var next_position : Vector3 = nav_agent.get_next_location()
+	var direction : Vector3 = next_position - current_position
+	var distance : float = current_position.distance_to(next_position)
+
+	if distance >= 0.1:
+		direction = direction.normalized()
+		velocity = direction * move_speed
+
+		# rotate_direction = Vector2(direction.x, direction.z).normalized()
+		look_at(next_position)
+
+		move_and_slide()
+
+
 func rotate_character(delta : float):
 	rotate_y(rotate_direction.x * -0.25 * delta)
-	pass
+	print(rotation)
+
+
+func set_new_nav_destination(new_destination : Vector3):
+	nav_destination = new_destination
+	nav_agent.set_target_location(new_destination)
