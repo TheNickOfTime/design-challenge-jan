34fdcfe2a4504de856f099a26fd3116fc731f152
Nick Cunningham <10342737+TheNickOfTime@users.noreply.github.com>
2023-01-16 17:42:15 -0800

    Added navigation support for non-primary characters.

--- Scripts/Camera.gd
+++ Scripts/Camera.gd
@@ -10,6 +10,10 @@ var pan_direction : int = -1
 var max_angle : float = 30
 
 
+@onready var camera : Camera3D = $Camera3D
+@onready var raycast : RayCast3D = $RayCast3D
+
+
 func _ready():
 	pass
 
@@ -23,7 +27,14 @@ func follow_camera(delta : float):
 	position = character.position + offset
 
 
+
 func rotate_camera(delta : float):
 	rotate_y(mouse_delta.x * pan_direction * pan_speed * delta)
 	rotate_object_local(Vector3.RIGHT, mouse_delta.y * pan_direction * pan_speed * delta)
 	rotation.x = clamp(rotation.x, deg_to_rad(-max_angle), deg_to_rad(max_angle))
+
+func get_position_from_raycast() -> Vector3:
+	# print($Camera3D.get_camera_transform().origin)
+	raycast.global_position = camera.get_camera_transform().origin
+	raycast.force_raycast_update()
+	return raycast.get_collision_point()
--- Scripts/Controller.gd
+++ Scripts/Controller.gd
@@ -3,20 +3,31 @@ class_name PlayerController
 
 var characters : Array[Node]
 var camera : PlayerCamera
+var camera_directions : Array[Transform3D]
 
-var character_index : float = 0
+var character_index : int = 0
 var move_direction : Vector2 = Vector2.ZERO
 var look_direction : Vector2 = Vector2.ZERO
 
-var character: PlayerCharacter:
+var current_character: PlayerCharacter:
 	get:
 		return characters[character_index] as PlayerCharacter
 
+var other_character: PlayerCharacter:
+	get:
+		var index = wrapi(character_index + 1, 0, characters.size())
+		return characters[index] as PlayerCharacter
 
+
+#Built In Functions---------------------------------------------------------------------------------
 func _ready():
 	characters = get_tree().get_nodes_in_group("Character")
 	camera = get_tree().get_first_node_in_group("Camera")
+	camera_directions.resize(2)
+	for i in camera_directions.size():
+		camera_directions[i] = camera.transform
 	
+	setup_player()
 	setup_camera()
 
 
@@ -36,30 +47,57 @@ func _input(event):
 	
 	if event.is_action_pressed("ui_cancel"):
 		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+	
+	if event.is_action_pressed("character_command"):
+		update_nav_destination()
 
 
 func input_poll():
 	move_direction = Input.get_vector("move_left", "move_right", "move_forward", "move_back")
 
 
+# Character Functions-------------------------------------------------------------------------------
 func switch_character():
+	# save previous values
+	camera_directions[character_index] = camera.transform
+
+	# set new values
 	character_index = wrapi(character_index + 1, 0, characters.size())
+	setup_player()
 	setup_camera()
 
 
 func setup_player():
-	pass
+	for character in characters:
+		character = character as PlayerCharacter
+		
+		if character == current_character:
+			character.control_state = PlayerCharacter.CONTROL_STATE.Input
+		else:
+			character.nav_destination = character.position
+			character.control_state = PlayerCharacter.CONTROL_STATE.Navigation
 
 
 func update_player():
-	character.move_direction = move_direction
-	character.rotate_direction = look_direction
+	current_character.move_direction = move_direction
+	current_character.rotate_direction = look_direction
 
 
+func update_nav_destination():
+	var new_destination = camera.get_position_from_raycast()
+	print(new_destination)
+	if new_destination != null:
+		other_character.set_new_nav_destination(new_destination)
+
+
+# Camera Functions----------------------------------------------------------------------------------
 func setup_camera():
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
-	camera.character = character
-	camera.global_rotation = character.global_rotation
+	camera.character = current_character
+	# camera.global_rotation = current_character.global_rotation
+	if camera_directions[character_index] != null:
+
+		camera.transform = camera_directions[character_index]
 
 
 func update_camera():
