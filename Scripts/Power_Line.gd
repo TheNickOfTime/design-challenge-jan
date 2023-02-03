74725d6bd82e11f7a34403b981dec1ff9c33a965
Nick Cunningham <thenickoftime.x@gmail.com>
2023-02-02 23:22:07 -0800

    Worked on powelines mostly

---
+++ Scripts/Power_Line.gd
@@ -0,0 +1,54 @@
+@tool
+extends Path3D
+class_name PowerLine
+
+
+const decal_spacing : float = 1
+
+@export var decal : PackedScene
+@export var activated_color : Color
+@export var deactivated_color : Color
+
+var decals : Array[Decal]
+
+
+func _enter_tree():
+	curve = Curve3D.new()
+	curve_changed.connect(_on_curve_changed)
+	update_decals()
+
+
+func _exit_tree():
+	pass
+
+
+func _on_curve_changed():
+	update_decals()
+
+
+func update_decals():
+	for item in decals:
+		item.queue_free()
+	
+	decals.clear()
+
+	for i in (curve.get_baked_length() / decal_spacing + 1) as int:
+		var new_decal = decal.instantiate()
+		new_decal.position = curve.sample_baked(i / decal_spacing)
+		new_decal.modulate = deactivated_color
+		decals.append(new_decal)
+		add_child(new_decal)
+
+func update_decal_colors(is_activated : bool, time : float):
+	if !is_activated:
+		decals.reverse()
+	
+	var color : Color = activated_color if is_activated else deactivated_color
+	var time_per :float = time / decals.size()
+
+	for item in decals:
+		item.modulate = color
+		await get_tree().create_timer(time_per).timeout
+	
+	if !is_activated:
+		decals.reverse()
