9417eaf3ea35d4e9117002df089682a4f55bdb92
Nick Cunningham <thenickoftime.x@gmail.com>
2023-02-21 22:34:19 -0800

    Attempted fix at some bugs with the platforms & power lines

--- Scripts/Puzzle Pieces/Triggerables/Triggerable_Platform.gd
+++ Scripts/Puzzle Pieces/Triggerables/Triggerable_Platform.gd
@@ -6,6 +6,8 @@ extends Triggerable
 @export var move_direction : Vector3 = Vector3(0, 1, 0)
 @export var platform : StaticBody3D
 
+var last_tween : Tween
+
 
 func trigger_activated():
 	tween_platform(true)
@@ -16,8 +18,13 @@ func trigger_deactivated():
 
 
 func tween_platform(is_up : bool):
+#	if last_tween != null:
+#		last_tween.stop()
+	
 	var tween : Tween = get_tree().create_tween()
 	var destination : Vector3 = move_direction.normalized() * move_distance if is_up else Vector3.ZERO
 	var tween_time : float = destination.distance_to(platform.position) / move_speed
 
 	tween.tween_property(platform, "position", destination, tween_time)
+	
+	last_tween = tween
